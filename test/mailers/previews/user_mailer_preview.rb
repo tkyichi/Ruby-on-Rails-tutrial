22252b47bd8ddfcafeb98121de9b6aa52314fb51
tkyichi <ichigitky@gmail.com>
2018-01-06 17:07:13 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,8 +1,8 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at http://rails-tutorial-tkyichi.c9users.io/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at
-  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # http://rails-tutorial-tkyichi.c9users.io/rails/mailers/user_mailer/account_activation
   def account_activation
     user = User.first
     user.activation_token = User.new_token
@@ -10,8 +10,10 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   # Preview this email at
-  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # http://rails-tutorial-tkyichi.c9users.io/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
